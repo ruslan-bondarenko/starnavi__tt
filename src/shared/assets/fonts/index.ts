f12c604e126c6112490d0e8a384c48efd56ab991
Ruslan Bondarenko <134707990+ruslan-bondarenko-fourmeta@users.noreply.github.com>
2024-03-30 15:45:02 +0200

    fix: resolved path issue

--- src/shared/assets/fonts/index.ts
+++ src/shared/assets/fonts/index.ts
@@ -3,7 +3,7 @@ import localFont from 'next/font/local';
 export const starwars = localFont({
  src: [
   {
-   path: './StarJedi.ttf',
+   path: './Starjedi.ttf',
    weight: '400',
    style: 'normal',
   },
